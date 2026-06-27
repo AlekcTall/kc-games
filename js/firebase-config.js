6ad0185f7aa9b712c7d24b4e55156bbfe70e956b
AlekcTall <Alekcey.gyngazov@mail.ru>
2026-06-27 15:14:54 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "...",  // твои данные
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBc46OF0mZJJE8CAqwqOxnw_QyZM9CuHQ4",
+  authDomain: "kc-gamification.firebaseapp.com",
+  projectId: "kc-gamification",
+  storageBucket: "kc-gamification.firebasestorage.app",
+  messagingSenderId: "12453241915",
+  appId: "1:12453241915:web:b27b5ed332918c3f8b681e"
 };
 
 // Инициализация Firebase
