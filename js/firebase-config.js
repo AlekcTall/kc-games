4b346f1922923448974d513749371dfb0bcb69b3
AlekcTall <Alekcey.gyngazov@mail.ru>
2026-06-27 15:16:47 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,6 +7,5 @@ const firebaseConfig = {
   messagingSenderId: "12453241915",
   appId: "1:12453241915:web:b27b5ed332918c3f8b681e"
 };
-
 // Инициализация Firebase
 firebase.initializeApp(firebaseConfig);
