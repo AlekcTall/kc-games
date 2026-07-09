f825824d85ef7908265ad3b55a4820afc5040b55
AlekcTall <Alekcey.gyngazov@mail.ru>
2026-07-09 21:59:08 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,25 +1,5 @@
 // js/storage.js
 
-// ================== ИГРЫ (localStorage) ==================
-function getGames() {
-  const data = localStorage.getItem('krugames_games');
-  return data ? JSON.parse(data) : [];
-}
-
-function saveGames(games) {
-  localStorage.setItem('krugames_games', JSON.stringify(games));
-}
-
-// ================== ПОЛЬЗОВАТЕЛИ (localStorage – резерв) ==================
-function getUsers() {
-  const data = localStorage.getItem('krugames_users');
-  return data ? JSON.parse(data) : [];
-}
-
-function saveUsers(users) {
-  localStorage.setItem('krugames_users', JSON.stringify(users));
-}
-
 // ================== ТЕКУЩИЙ ПОЛЬЗОВАТЕЛЬ (кеш) ==================
 function getCurrentUser() {
   const data = localStorage.getItem('krugames_currentUser');
